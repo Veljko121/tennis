e9676bcade155ac60e80d52dd99d222764b8d0eb
Veljko Nikolić <nikolicveljko01@gmail.com>
2024-06-14 17:59:00 +0200

    Added commands for creating indexes

---
+++ index_commands.js
@@ -0,0 +1,21 @@
+db.matches.createIndex(
+    { 'round': 1 },
+    { partialFilterExpression: { 'round': { '$in': [ 'F', 'SF' ] } } }
+);
+
+db.matches.createIndex(
+    { 'tournament.level': 1 },
+    { partialFilterExpression: { 'tournament.level': { '$in': [ 'G', 'M', 'F' ] } } }
+);
+
+db.matches.createIndex(
+    { 'winner.id': 1 }
+);
+
+db.matches.createIndex(
+    { 'loser.id': 1 }
+);
+
+db.rankings.createIndex(
+    { 'rank': 1 }
+);
